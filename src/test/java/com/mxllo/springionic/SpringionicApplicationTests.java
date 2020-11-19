c94e0512604a20692bac14ed218df48084b4e8f5
Lucas Mello <lucasmellog@gmail.com>
2020-11-19 10:41:45 -0300

    Criação do projeto

---
+++ src/test/java/com/mxllo/springionic/SpringionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxllo.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringionicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
