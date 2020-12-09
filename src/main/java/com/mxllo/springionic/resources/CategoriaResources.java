ef161a0709a4686eb065352f693315822e8d4a56
Lucas Mello <lucasmellog@gmail.com>
2020-12-09 16:57:23 -0300

    Rest controller tested

--- src/main/java/com/mxllo/springionic/resources/CategoriaResources.java
+++ src/main/java/com/mxllo/springionic/resources/CategoriaResources.java
@@ -1,4 +1,4 @@
-package resources;
+package com.mxllo.springionic.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
