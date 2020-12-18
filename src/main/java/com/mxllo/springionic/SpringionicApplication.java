7420b28c164f3620b540c11ef68cbe18d2282a42
Lucas Mello <lucasmellog@gmail.com>
2020-12-18 12:11:26 -0300

    Criando instaciação

--- src/main/java/com/mxllo/springionic/SpringionicApplication.java
+++ src/main/java/com/mxllo/springionic/SpringionicApplication.java
@@ -1,13 +1,28 @@
 package com.mxllo.springionic;
 
+import com.mxllo.springionic.model.Categoria;
+import com.mxllo.springionic.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringionicApplication {
+public class SpringionicApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringionicApplication.class, args);
     }
 
+    @Override
+    public void run(String... args){
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        repo.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
